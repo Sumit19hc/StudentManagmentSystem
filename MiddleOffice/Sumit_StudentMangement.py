4e28b15a20032d5f1356148ace045882f5a43a11
sumit <sumitparate30@gmail.com>
2022-04-18 07:15:58 +0530

    Database

--- MiddleOffice/Sumit_StudentMangement.py
+++ MiddleOffice/Sumit_StudentMangement.py
@@ -1,16 +1,37 @@
-def add():
+def createRegister():
+    f = open("studentsInfo.txt", "a")
+    return f
+
+
+def WriteToRegister(f, name, age, phone_number):
+    f.write("{}|{}|{}\n".format(name, age, phone_number))
+
+def readRegister():
+    f = open("studentsInfo.txt","r")
+    print(f.read())
+def checkifStudentsExits(newStudentName):
+    readf = open("studentsInfo.txt", "r+t")
+    if newStudentName == name:
+        print("student ={} already exist".format(newStudentName))
+
+
+def add(f):
     name = input("Enter Name :- ")
     age = int(input("Enter Age :- "))
     phone_number = int(input("Enter Phone Number :-"))
+    WriteToRegister(f, name, age, phone_number)
     print("New Student is added")
+
 
 def delete():
     name = input("Enter Name of Student:- ")
     print("Delete Student")
 
+
 def search():
     name = input("Enter Name of Student:- ")
     print("It should display student Name", name)
+
 
 def header():
     print('''********* \nWainganga \n***********''')
@@ -21,11 +42,14 @@ def choice():
     choice = int(input("Enter You choice :- "))
     return choice
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    f = createRegister()
     cho = choice()
     if cho == 1:
         header()
-        add()
+        add(f)
+        readRegister()
     elif cho == 2:
         header()
         delete()
@@ -34,4 +58,3 @@ if __name__=="__main__":
         search()
     else:
         print("Invalid Choice")
-
